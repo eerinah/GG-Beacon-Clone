e1095a536c1f796b4fa5557f79710170382adf74
riley <alkalinarwhal@gmail.com>
2023-06-04 18:13:22 -0700

    0005
    changed

--- responses.py
+++ responses.py
@@ -3,5 +3,7 @@ def handle_response(message) -> str:
     #can preprocess the message and then maybe run the model on it
     p_message = message.lower()
 
-    #return this
+    #based on what the model returns,
+    #we prefix the message with 0 (no problem), 1 (hate), or 2 (depressive)
+    p_message = '0' + p_message
     return p_message
